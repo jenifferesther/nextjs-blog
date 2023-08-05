62cb3bc0c5da13f23be717ea807437694f2bb812
jeniffer esther <jes21@aluno.ifal.edu.br>
2023-08-05 11:18:57 -0300

    welcome >>learn

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+npmnpm
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
