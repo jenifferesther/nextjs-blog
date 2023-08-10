b4c812a52e4c198a6f521d228b79a637aa0e3130
jeniffer esther <jes21@aluno.ifal.edu.br>
2023-08-10 16:33:53 -0300

    adiciona Head nas pág/profile e /post/first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from "next/link"
+import Head from 'next/head'
 
 export default function FirstPost(){
     return (
         <> 
+        <Head>
+            <title> first post</title>
+        </Head>
+
         <h1>First Post</h1>
         <h2>
             <Link href="/"> Back to Home </Link>
--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,21 @@
 import Image from'next/image';
+import Head from 'next/head';
 
 export default function Profile(){
 
-return <Image src="/images/profile.jpg"
-height={144} 
-width={144}
-alt='HOUVE UM ERRO'
-/>
+return (
+<>
+    <Head>
+    <title>Profile</title>
+    </Head>
 
+
+    <Image 
+    src="/images/profile.jpg"
+    height={144} 
+    width={144}
+    alt='HOUVE UM ERRO'
+/>
+</>
+);
 }
