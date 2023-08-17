5bd05a484100f0414e616cd4119656bcad86cfce
jeniffer esther <jes21@aluno.ifal.edu.br>
2023-08-17 15:50:09 -0300

    adcionando css no layout

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,6 @@
-export default function({children}){
-    return <div>{children}</div>;
+import styles from './layout.module.css'
+
+export default function Layout({children}){
+    
+    return <div className={styles.container}>{children}</div>;
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,12 @@
 import Link from "next/link"
 import Head from 'next/head'
 import Script from "next/script"
+import Layout from "../../components/layout";
 
 export default function FirstPost(){
     return (
         <> 
+        <Layout>
         <Head>
             <title> first post</title>
         </Head>
@@ -21,8 +23,9 @@ export default function FirstPost(){
         <h2>
             <Link href="/"> Back to Home </Link>
         </h2>
-        
+        </Layout>
         </>
+        
     );
 
 }
