33dd8d4f787d5b9ad27c3fc8c94f2b29616954c4
jeniffer esther <jes21@aluno.ifal.edu.br>
2023-08-24 14:21:02 -0300

    criando /second-post

---
+++ pages/posts/Second-post.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+import Layout from "../../components/layout"
+
+export default function SecoudPost(){
+
+    return (
+        <Layout>
+         <Head>
+            <title>
+                SecoudPost
+            </title>
+         </Head>
+         <h1>Secoud Post</h1>
+        </Layout>
+        
+    )
+}
