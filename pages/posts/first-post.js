8095f04de1cbe6ed6db7ebb7de5b5d4f3afd376a
jeniffer esther <jes21@aluno.ifal.edu.br>
2023-08-24 16:27:44 -0300

    alteraçao de index e layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -20,9 +20,7 @@ export default function FirstPost(){
         />
       
         <h1>First Post</h1>
-        <h2>
-            <Link href="/"> Back to Home </Link>
-        </h2>
+        
         </Layout>
         </>
         
